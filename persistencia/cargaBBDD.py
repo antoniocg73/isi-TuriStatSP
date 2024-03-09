be7cb821847a09d1c0255a0ae7e8f5b6b634ccb1
Antonio Campallo Gómez <ancago4@gmail.com>
2024-03-09 18:11:07 +0100

    BBDD del CSV

---
+++ persistencia/cargaBBDD.py
@@ -0,0 +1,44 @@
+#Instalar sqlite3 de la pagina oficial: https://www.sqlite.org/download.html
+#Instalar sqlitebrowser de la página oficial: https://sqlitebrowser.org/dl/
+import sqlite3
+import csv
+import os
+
+# Ruta del archivo CSV
+csv_file_path = os.path.join("persistencia", "DatosINE.csv")
+# Conectar a la base de datos SQLite
+conn = sqlite3.connect('persistencia/TuriStatSP-BBDD.db')
+cursor = conn.cursor()
+
+# Crear la tabla si no existe
+cursor.execute('''CREATE TABLE IF NOT EXISTS TuriStatSP_BBDD (
+                    Total_Nacional TEXT,
+                    Comunidades_Ciudades_Autonomas TEXT,
+                    Provincias TEXT,
+                    Residencia_del_viajero TEXT,
+                    Periodo INTEGER,
+                    Total INTEGER
+                )''')
+
+# Leer los datos del archivo CSV y cargarlos en la base de datos
+with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=';')
+    next(csvreader)  # Saltar la primera fila si contiene encabezados
+
+    for row in csvreader:
+        Total_Nacional, Comunidades_Ciudades_Autonomas, Provincias, Residencia_del_viajero, Periodo, Total = row
+        # Reemplazar '..' con None para manejar los valores faltantes
+        if Total == '..':
+            Total = None
+        else:
+            Total = int(Total.replace(".", "")) # Convertir el total a entero
+        
+        # Insertar fila en la base de datos
+        cursor.execute('''INSERT INTO TuriStatSP_BBDD VALUES (?, ?, ?, ?, ?, ?)''', 
+                       (Total_Nacional, Comunidades_Ciudades_Autonomas, Provincias, Residencia_del_viajero, Periodo, Total))
+
+# Confirmar los cambios y cerrar la conexión
+conn.commit()
+conn.close()
+
+print("Los datos se han cargado correctamente en la base de datos SQLite.")
