2a8cd845d7abcc7e6aa49bd6b89d7f6228741581
Antonio Campallo Gómez <ancago4@gmail.com>
2024-03-09 17:40:17 +0100

    representacion mapa OpenStreetMap, carga API INE

---
+++ presentacion/gestionMapa/mapa.js
@@ -0,0 +1,5 @@
+let map = L.map('map').setView([4.639386, -74.082412],6)
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
