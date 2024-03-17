254023a756b3ead16ad63f9d93a3c9718cbdafe2
Antonio Campallo Gómez <ancago4@gmail.com>
2024-03-17 11:17:13 +0100

    DatosIne Actualizado

--- persistencia/DatosApiINE.py
+++ persistencia/DatosApiINE.py
@@ -18,21 +18,3 @@ nombre_archivo_destino = 'persistencia/DatosINE.csv'
 descargar_archivo(url, nombre_archivo_destino)
 
 
-# Obtener la última fecha de actualización de los datos
-url = 'https://datos.gob.es/es/catalogo/ea0010587-datos-de-ocupacion-en-alojamientos-turisticos-y-otros-alojamientos-de-corta-estancia-anual-oat-identificador-api-48440'
-
-respuesta = requests.get(url)
-
-if respuesta.status_code == 200:
-
-    soup = BeautifulSoup(respuesta.text, 'html.parser')
-    
-    elemento_fecha = soup.find('span', class_='automatic-local-datetime')
-    
-    if elemento_fecha and elemento_fecha.has_attr('data-datetime'):
-        fecha_actualizacion = elemento_fecha['data-datetime']
-        print('Fecha de última actualización (ISO 8601):', fecha_actualizacion)
-    else:
-        print('No se encontró la fecha de última actualización.')
-else:
-    print('Error al realizar la solicitud a la página:', respuesta.status_code)
